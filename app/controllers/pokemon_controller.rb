86a11090782c1ad381494df41c59ebc0b214d2a0
Sclarke5 <sclarke5555@gmail.com>
2018-05-08 17:13:06 -0400

    adds search by either id or name

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,14 +1,50 @@
 class PokemonController < ApplicationController
   def show
-    res = HTTParty.get("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
-    body = JSON.parse(res.body)
-    id = params[:id]
+    pokedex_res = HTTParty.get("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json")
+    pokedex_body = JSON.parse(pokedex_res.body)
+
+    input = params[:id]
 
 
+    pokedex_body["pokemon"].each do |i|
+      if input.to_i == i["id"]
+        @pokemon_id = i["id"]
+        @pokemon_name = i["name"]
+        @pokemon_type = i["type"]
+      elsif input.to_i == 0
+        if input.capitalize == i["name"]
+          @pokemon_id = i["id"]
+          @pokemon_name = i["name"]
+          @pokemon_type = i["type"]
+        end
+      end
 
-    render json: {    "id": body["pokemon"][id.to_i-1]["id"],
-                      "name": body["pokemon"][id.to_i-1]["name"],
-                      "types": body["pokemon"][id.to_i-1]["type"]
-                      }
+
+    # if newID > 0
+    # @found = pokedex_body["pokemon"].find_by(id: "id")
+    # @found_id = @found['id']
+    # @pokemon_id = pokedex_body["pokemon"][id.to_i-1]["id"]
+    # @pokemon_name = pokedex_body["pokemon"][id.to_i-1]["name"]
+    # @pokemon_type = pokedex_body["pokemon"][id.to_i-1]["type"]
+    # else
+    # @pokemon_id = pokedex_body["pokemon"]['id']["id"]
+    # @pokemon_name = pokedex_body["pokemon"]['name']["name"]
+    # @pokemon_type = pokedex_body["pokemon"]['name']["type"]
+    end
+
+    name = @pokemon_name
+    key = ENV["GIPHY_KEY"]
+    gif_response = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{name}&rating=g")
+    gif_body = JSON.parse(gif_response.body)
+    @gif_url = gif_body["data"][0]["images"]["original"]["url"]
+
+    # render json: {    "id": @pokemon_id,
+    #                   "name": @pokemon_name,
+    #                   "types": @pokemon_type,
+    #                   "gif": @gif_url}
+
+
   end
 end
+
+# if to_i > 0
